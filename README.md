099c24658d61a3652557b71b8ffd0dd3356fce31
kweya <kweyaanton@gmail.com>
2024-12-05 18:53:50 +0300

    state updates

---
+++ README.md
@@ -0,0 +1,25 @@
+# Sort Visualizer
+
+## Features
+
+1. Choose visualization speed from the app header.
+2. Select the array size using a user-friendly slider.
+3. Ability to generate new random array that is rendered to produce another set of div bars.
+4. Select sorting algorithm.
+5. All features are disabled during sorting visualization with the use of Promises.
+
+## Project Setup
+
+```
+cd sort-visualizer
+npm install
+npm start
+```
+
+## Built with
+
+- JavaScript
+- JSX
+- [React](https://reactjs.org/)
+- [Tailwind CSS](https://tailwindcss.com/)
+- CSS3
---
+++ src/App.js
@@ -0,0 +1,86 @@
+import React, { useEffect, useRef, useState } from "react";
+import MyFooter from "./components/MyFooter";
+import MyHeader from "./components/MyHeader";
+import Content from "./components/Content";
+
+import {
+  loadDivs,
+  bubbleSort,
+  insertionSort,
+  mergeSort,
+  quickSort,
+  changeSpeed,
+} from "./logic/sortLogic";
+
+function App() {
+  const [currArr, setCurrArr] = useState([]);
+  const barsArrayRef = useRef(null);
+  
+    useEffect(() => {
+      //load up bar divs every time currArr changes.
+      loadDivs(barsArrayRef.current?.children);
+    }, [currArr]);
+  
+
+  // randomizes the array
+  const randomizeArray = (newLength = currArr.length) => {
+    //generate an array of numbers less than 100
+    let tmpArr = [];
+    for (let i = 0; i < newLength; i++) {
+      tmpArr.push(Math.floor(Math.random() * (100 - 10 + 1)) + 10);
+    }
+
+    setCurrArr([...tmpArr]);
+  };
+
+  const handleChangeSpeed = (newSpeed) => {
+    switch (newSpeed) {
+      case "fast":
+        changeSpeed(10);
+        break;
+
+      case "medium":
+        changeSpeed(50);
+        break;
+
+      case "slow":
+        changeSpeed(100);
+        break;
+
+      default:
+        break;
+    }
+  };
+
+  async function runBubble() {
+    const newArr = await bubbleSort(currArr);
+    setCurrArr(newArr);
+  }
+
+  async function runInsertion() {
+    const newArr = await insertionSort(currArr);
+    setCurrArr(newArr);
+  }
+
+  async function runMerge() {
+    const newArr = await mergeSort(currArr);
+    setCurrArr(newArr);
+  }
+
+  async function runQuick() {
+    const newArr = await quickSort(currArr);
+    setCurrArr(newArr);
+  }
+
+  return (
+    <div className="min-h-screen w-full flex flex-col items-center">
+      <MyHeader bubble={runBubble} insertion={runInsertion} merge={runMerge} quick={runQuick} randomize={randomizeArray} changeSpeed={handleChangeSpeed}/>
+
+      <Content arr={currArr} barsRef={barsArrayRef} />
+
+      <MyFooter />
+    </div>
+  );
+}
+
+export default App;
